6edb3920e6331e1cd29930dc4641d9bc2b6d18ed
Tristan Elliott <47083513+thePlebDev@users.noreply.github.com>
2020-04-09 12:25:12 -0300

    Delete apiKeys.js

--- src/components/keys/apiKeys.js
+++
@@ -1,8 +0,0 @@
-var API_KEY = 'AIzaSyBPtG_u3vNKij2Ubxyso1SOa7bBoGibgDk'
-var CLIENT_ID ='176817152233-jhcrvsk5qrisugqtu2c50nbbcummbqq4.apps.googleusercontent.com'
-
-
-// the CLIENT_ID is only required if we want to make authorized calls to the google API and get users personal information
-// we will do this eventualy but not right now.
-
-export default API_KEY
